1c882e5d90b536ab97ae3a9273a165c8a090de0f
Nicholas Olds <nolds9@yahoo.com>
2015-11-02 00:43:59 -0500

    NHO: feedback on QT Project 1

---
+++ feedback_nho.md
@@ -0,0 +1,27 @@
+# Project Feedbac
+
+## Project Workflow
+
+>Did you complete the user stories, wireframes, task tracking, and/or ERDs, as specified above? Did you use source control as expected for the phase of the program you’re in (detailed above)?
+
+**Meets Expectations**. Your project is falls appropriately into the scope of a first project and your user stories show that. In the future, it would be nice to see more thoughts from your planning process, like a trello board with current, backlog, and icebox user stories, and relevant wireframes.
+
+## Technical Requirements
+
+>Did you deliver a project that met all the technical requirements? Given what the class has covered so far, did you build something that was reasonably complex?
+
+**Meets Expectations**. Congratulations.  All the concepts covered during the first few week of WDI are represented.  You have tackled some complex problems. I recommend you come back to this and I bet you'd be surprised how much clearer it will be with a fresh perspective.
+
+## Code Quality
+
+>Did you follow code style guidance and best practices covered in class, such as spacing, modularity, and semantic naming? Did you comment your code as your instructors have in class?
+
+**Meets Expectations**. Html is clearly structured, CSS is clear, and JS works, I recommend you check your code in a validator.  Make sure to clean up code, remove comments, fix indentation in the future. Moving forward, try to watch for duplication and areas where you can group functionality into functions.
+
+[inline code comments here](https://github.com/qadriyyah119/WDI-Project-1compare/master...nolds9:feedback_nho)
+
+## Problem Solving
+
+>Are you able to defend why you implemented your solution in a certain way? Can you demonstrate that you thought through alternative implementations?
+
+**Meets Expectations **. Excellent excellent job on this project! I would really like to see you implement the features letting users know their score and when they got their turn wrong.
--- script.js
+++ script.js
@@ -1,88 +1,56 @@
-$(document).ready(function(){
-  clickcounter = 0
-$(".square").on("click", function(){
-  clickcounter++
-  console.log(clickcounter)
-  console.log($(this).find("img").css("display"))
-  $(this).find("img").toggle();
-  console.log($(this).find("img").css("display"))
-  if (clickcounter % 2 == 0 ){
-    console.log("if conditional worked")
-    //run checkmatch function
-    checkMatch()
-  }
+// NHO: ran JS code through Beautify. Why is it easier to maintain clean code?
+$(document).ready(function() {
+  clickcounter = 0 // NHO: missing var keyword, this is a global variable
+  $(".square").on("click", function() {
+    clickcounter++
+    console.log(clickcounter) // NHO: remove console.logs and debugging lines for submissions
+    console.log($(this).find("img").css("display"))
+    $(this).find("img").toggle();
+    console.log($(this).find("img").css("display"))
+    if (clickcounter % 2 == 0) {
+      console.log("if conditional worked")
+        //run checkmatch function
+      checkMatch() // nice!
+    }
   });
-  //if (click % 2 == 0 ){
+  // =============== NHO: Remove commented out/unused code =======================
 
-
-    //run checkmatch function if clicked in multiples of two
-    //checkmatch will given the two cards clicked compare the image source attribute if they match perm keep face up, otherwise flip them both back over.
-    //checkwinner function will alert that you won if all the cards are face up
-    //event.target should be pushed in an array
-  //}
-
-
-    function checkMatch(){
-      var flippedImages = $("img:visible")
-      console.log(flippedImages)
-      var firstImage = flippedImages[0]
-      var secondImage = flippedImages[1]
-      if (firstImage.src == secondImage.src){
-        $(firstImage).css("display","inline")
+  function checkMatch() {
+    var flippedImages = $("img:visible") // NHO: is this selector working the way you want it to?
+    console.log(flippedImages)
+    var firstImage = flippedImages[0]
+    var secondImage = flippedImages[1]
+    if (firstImage.src == secondImage.src) { // NHO: this throws an error in the console when no squares are flipped
+      $(firstImage).css("display", "inline")
         //this is a match
         //squares should stay shown
         //display:inline
-      } else {
-        //not a match
-        //squares should say hidden.
-        //display should be none
-
-      }
+    } else {
+      //not a match
+      //squares should say hidden.
+      //display should be none
+      // NHO: Good job breaking down the steps! I encourage you to come back to this and work through the matching functionality!
     }
+  }
 
 
+  // NHO: It's a good idea to get into the habit of organizing or commenting out blocks of related code.
+  var seconds = 0
 
-    var seconds = 0
+  var timerId;
 
-    var timerId;
+  var startButton = document.getElementById("reset")
 
-    var startButton = document.getElementById("reset")
-    function updateTime(){
+  function updateTime() {
     seconds++
-    document.getElementById("reset").innerHTML=seconds
-    }
-    startButton.addEventListener("click", function(){
-      updateTime()
-      timerId = setInterval(updateTime,1000)
-    })
+    document.getElementById("reset").innerHTML = seconds
+  } // NHO: Let's make sure our code's indentation is consistent.
+  startButton.addEventListener("click", function() {
+    updateTime()
+    timerId = setInterval(updateTime, 1000)
+  })
 
 });
-//when the counter if it equals 2 that's when checks if it's a correct match or nah
 
-
-//create a click counter
-//the counter will count the number of clicks and only allow
-
-/*$(".square").on('click', function() {
-    //var click = +$(this).data('clicks') || 0;
-    if (click % 2 == 0) {
-        $(this).children().show();
-    }else{
-        $(this).children().hide();
-    };
-    $(this).data('clicks',click+1);
-  });
-  */
-
-/*var A = 'nike1.jpg';
-var B = 'nike2.jpg';
-var C = 'nike3.jpg';
-var D = 'nike4.jpg';
-var E = 'nike5.jpg';
-var F = 'nike6.jpg';
-var G = 'nike7.jpg';
-var H = 'nike8.jpg';
-var I = 'nike9.jpg';
-var J = 'nike10.jpg';
-var K = 'nike11.jpg';
-var L = 'nike12.jpg';*/
+// =============== NHO: Remove commented out/unused code =======================
+// =============== NHO: See further comments in feedback.md ! ====================
